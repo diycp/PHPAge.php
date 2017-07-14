005a290a54ba26b9cf47b33322414f70d76cd07c
calvin.Chen <injxnu@163.com>
2017-07-14 11:43:54 +0800

    update code

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
  require_once 'PHPAge.php';
- 
+ header("Content-Type:text/html;charset=utf-8;");
  $age=new Age('./test.jpg');//Pictures of faces
  $result=$age->age();
- echo "<img src='./test.jpg' width=300px height=400px />";
- echo "<br/>age:".$result;
+ $jsonarr=json_decode($result,true);
+ if($jsonarr['errno']==0){
+	 
+	echo "<img src='./test.jpg' width=300px height=400px /><br/>";
+	echo "json包:";
+	print_r($result);
+	echo "<br/>";
+	foreach($jsonarr['age'] as $k=>$v){
+	echo "年龄".$k.":".$v;
+	}
+	echo "数量:".$jsonarr['number'];
+	echo "脸部区域X:".$jsonarr['rect'][0];
+	echo "脸部区域Y:".$jsonarr['rect'][1];
+	echo "脸部区域H:".$jsonarr['rect'][2];
+	echo "脸部区域W:".$jsonarr['rect'][3];
+ }
+ 
 
 ?>
